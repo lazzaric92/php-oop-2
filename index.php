15d9119785be4867da32415e57a6c74b724ff0b1
lazzaric92 <lazzaric_92@hotmail.it>
2024-07-03 23:44:31 +0200

    added icons and p according to Class

--- index.php
+++ index.php
@@ -7,7 +7,11 @@ require_once __DIR__ . "/db/db.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Font-awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- CSS -->
     <link rel="stylesheet" href="./css/style.css">
     <title>Boolshop</title>
 </head>
@@ -28,17 +32,28 @@ require_once __DIR__ . "/db/db.php";
                             </div>
                             <div class="card-body">
                                 <h5 class="card-title mb-2 fw-bold"><?php echo $product->getName() ?></h5>
-                                <h6 class="card-subtitle mb-2"><?php echo $product->getCategory()->getName() ?></h6>
-                                <p class="card-text">Prezzo: &euro; <?php echo $product->getPrice() ?></p>
+                                <h6 class="card-subtitle mb-3">
+                                    <?php if(get_class($product->getCategory()) === "Dog"){ ?>
+                                        <span><i class="fa-solid fa-dog"></i></span>
+                                    <?php } elseif(get_class($product->getCategory()) === "Cat"){ ?>
+                                        <span><i class="fa-solid fa-cat"></i></span>
+                                    <?php } elseif(get_class($product->getCategory()) === "Bird"){ ?>
+                                        <span><i class="fa-solid fa-dove"></i></span>
+                                    <?php } elseif(get_class($product->getCategory()) === "Fish"){ ?>
+                                        <span><i class="fa-solid fa-fish"></i></span>
+                                    <?php } ?>
+                                    <?php echo $product->getCategory()->getName() ?>
+                                </h6>
+                                <p class="card-text mb-2">Prezzo: &euro; <?php echo $product->getPrice() ?></p>
                                 <?php if(get_class($product) === "Food"){ ?>
-                                    <p class="card-text">Peso netto: <?php echo $product->getNetWeight() ?>g</p>
-                                    <p class="card-text">Ingredienti: <?php echo $product->getIngredients() ?></p>
+                                    <p class="card-text mb-2">Peso netto: <?php echo $product->getNetWeight() ?>g</p>
+                                    <p class="card-text mb-2">Ingredienti: <?php echo $product->getIngredients() ?></p>
                                 <?php } elseif(get_class($product) === "Toy"){ ?>
-                                    <p class="card-text">Caratteristiche: <?php echo $product->getFeatures() ?></p>
-                                    <p class="card-text">Dimensioni: <?php echo $product->getSize() ?></p>
+                                    <p class="card-text mb-2">Caratteristiche: <?php echo $product->getFeatures() ?></p>
+                                    <p class="card-text mb-2">Dimensioni: <?php echo $product->getSize() ?></p>
                                 <?php } elseif(get_class($product) === "Accessory"){ ?>
-                                    <p class="card-text">Materiale: <?php echo $product->getMaterial() ?></p>
-                                    <p class="card-text">Dimensioni: <?php echo $product->getSize() ?></p>
+                                    <p class="card-text mb-2">Materiale: <?php echo $product->getMaterial() ?></p>
+                                    <p class="card-text mb-2">Dimensioni: <?php echo $product->getSize() ?></p>
                                 <?php } ?>
                             </div>
                         </div>
